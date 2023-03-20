74ef403612ec41f662b4d4c994b298610561169b
WetTuna <51150463+WetTuna@users.noreply.github.com>
2023-03-19 20:34:39 -0700

    Update Create_a_Playlist.py

--- Create_a_Playlist.py
+++ Create_a_Playlist.py
@@ -247,7 +247,7 @@ def main():
         while True:
             try:
                 playlist_length = int(input("\nEnter the playlist length (Max 50): "))
-                if playlist_length <= 50:
+                if playlist_length <= 50 and playlist_length > 0:
                     break
                 print("Maximum of 50 songs is allowed.")
             except ValueError:
